9cbac5afe021736d63e24344627aaef451c59f66
Ken Shirriff <web1@righto.com>
2013-08-18 23:25:11 -0700

    Support comment lines in source, more comments.

--- sourceWindow.js
+++ sourceWindow.js
@@ -8,18 +8,25 @@ var SourceWindow = function(elem, model, sourceCode) {
   var height = 336 / 12; // height in lines
   var top = 0;
   var highlighted = null;
+  var addrToLine = [];
 
   // Copy code into source window
   (function init() {
     var html = [];
+    var regex = /^([\d ]{3}) (.*)/;
     for (var i = 0; i < sourceCode.length; i++) {
-      var addr = ('000' + i.toString(16)).substr(-3);
-      var parts = sourceCode[i].split('; ');
-      var instr = parts[0];
-      instr = (instr + '                  ').slice(0, 25);
-      var comment = parts.length > 1 ? parts[1] : null
-      var line = '<div id="s' + i + '">' +
-	'<span class="instr">' + instr + '</span>';
+      var comment = sourceCode[i].replace(';', '');
+      var line = '<div id="s' + i + '">';
+      var m = sourceCode[i].match(regex);
+      if (m) {
+	addrToLine[parseInt(m[1], 10)] = i;
+        var addr = ('000' + m[1]).substr(-3);
+	var parts = m[2].split('; ');
+	var instr = parts[0];
+	instr = (instr + '                  ').slice(0, 25);
+	var comment = parts.length > 1 ? parts[1] : null
+	line += '<span class="instr">' + instr + '</span>';
+      }
       if (comment) {
 	line += '<span class="comment">' + comment + '</span>';
       }
@@ -34,11 +41,12 @@ var SourceWindow = function(elem, model, sourceCode) {
     if (!model.power) {
       return;
     }
-    $('#s' + model.address).addClass('highlight');
-    highlighted = model.address;
+    highlighted = addrToLine[model.address];
+    $('#s' + highlighted).addClass('highlight');
 
-    if (model.address >= top + height || model.address < top) {
-      top = model.address - 3;
+    top = elem.scrollTop() / 12;
+    if (highlighted >= top + height || highlighted < top) {
+      top = highlighted - 3;
       if (top < 0) {
 	top = 0;
       }
